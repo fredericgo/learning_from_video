cb90d7d75b87df8ba0c3196f9c0f95914619befe
frederic go <fredericstgo@gmail.com>
2018-10-08 21:42:02 +0800

    plot

--- hmr/src/SkeletonExtractor.py
+++ hmr/src/SkeletonExtractor.py
@@ -81,19 +81,25 @@ class SkeletonExtractor:
         z_pelvis = (z[2] + z[3]) / 2.
         v_orig_l = normalize(np.array([0, 0.01, -.34]))
         v_new_g = normalize(z[1] - z[2])
-        v_y = normalize(z[3] - z[2]).tolist()
-        v_z = normalize(z[12] - z_pelvis).tolist()
-        v_x = normalize(normal_vector(v_y, v_z)).tolist()
+        v_y = normalize(z[3] - z[2])
+        v_z = normalize(z[12] - z_pelvis)
+        v_x = normalize(normal_vector(v_y, v_z))
         R = populateMatrix(v_x, v_y, v_z)
         v_new_l = R.transpose().dot(v_new_g)
 
+        def zVec(v):
+            return [0, 0, 0] + v.tolist()
+
         fig = plt.figure()
         ax = fig.add_subplot(111, projection='3d')
-        ax.quiver(*xo)
-        ax.quiver(*v_x, color='red')
-        ax.quiver(*a1,color='g')
-        ax.quiver(*a2,color='g')
+        ax.quiver(*zVec(v_x),color='red')
+        ax.quiver(*zVec(v_y),color='green')
+        ax.quiver(*zVec(v_z),color='blue')
+        ax.scatter(*z[2])
+        ax.scatter(*z[3])
+        ax.quiver(*zVec(v_new_g),color='yellow')
 
+        print(v_new_l)
         ax.set_xlim(-1,1)
         ax.set_ylim(-1,1)
         ax.set_zlim(-1,1)
