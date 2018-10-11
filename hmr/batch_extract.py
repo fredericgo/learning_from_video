8ec0dc0b9baf2dd8766967b8476e56bb48efe120
frederic go <fredericstgo@gmail.com>
2018-10-11 11:51:55 +0800

    batch extract

--- hmr/batch_extract.py
+++ hmr/batch_extract.py
@@ -1,4 +1,5 @@
 from absl import flags
+import os
 import src.config
 import sys
 from src.SkeletonExtractor import SkeletonExtractor
@@ -14,13 +15,19 @@ e = SkeletonExtractor(config)
 img_dir = "data/youtube/1/"
 onlyfiles = [f for f in os.listdir(img_dir)
              if os.path.isfile(os.path.join(img_dir, f))]
+onlyfiles = sorted(onlyfiles, 
+                   key=lambda f: int(f.rsplit('.')[0].split('_')[-1]))
 
 print("Extracting from directory: {}".format(img_dir))
-for file in onlyfiles:
-    img_path = os.path.join(img_dir, file)
-    z = e(f)
-    data = e.kinematicTree(z)
-    print(data)
+N = len(onlyfiles)
+X = np.zeros((N,24))
 
-#np.save('k_tree.npy', data)
+for i, file in enumerate(onlyfiles):
+    print("File: {}".format(file))
+    img_path = os.path.join(img_dir, file)
+    z = e(img_path)
+    data = e.kinematicTree(z)
+    X[i] = data
+
+np.save('results/k_trees.npy', data)
 #np.save('j3d.npy', z)
