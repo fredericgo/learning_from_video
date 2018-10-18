c930bed73e89686e114357e8978ea5f09d2c5f87
frederic go <fredericstgo@gmail.com>
2018-10-18 15:42:28 +0800

    l hip

--- hmr/test_quaternion.py
+++ hmr/test_quaternion.py
@@ -4,6 +4,8 @@ import sys
 from src.SkeletonExtractor import SkeletonExtractor
 import numpy as np
 import transforms3d
+from transforms3d import quaternions, euler
+
 
 config = flags.FLAGS
 config(sys.argv)
@@ -14,12 +16,17 @@ e = SkeletonExtractor(config)
 f = './data/coco1.png'
 z, theta = e(f)
 theta = theta.reshape((-1,3))
-x = theta[4]
+print(theta)
+x = theta[1]
 th = np.linalg.norm(x)
 x[0], x[1], x[2] = x[2], x[0], x[1]
 x /= th
-q = transforms3d.euler.axangle2euler(x, th, 'sxyz')
+print(x)
+print(th)
+q = quaternions.axangle2quat(x, th)
+a = euler.quat2euler(q)
 print(q)
+print(a)
 
 #np.save('results/k_tree.npy', data)
 #np.save('j3d.npy', z)
