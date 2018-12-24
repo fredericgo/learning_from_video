c472d801ebac21e1a3bae86b6b29c9a0bde400ec
Frederic Go <fredericstgo@gmail.com>
2018-12-24 13:31:09 +0800

    convert

--- video/convert.sh
+++ video/convert.sh
@@ -1,2 +1,2 @@
-ffmpeg -y -ss 00:00:36 -t 00:00:40 -i raw/videoplayback.mp4 -r 50.0 -vf scale=640:480 frames/skating/skating_%5d.jpg 
+ffmpeg -y -ss 00:01:48.3 -t 00:00:04 -i data/raw/pitch2.mp4 -r 50.0 -vf scale=640:480 data/frames/pitch2/img_%5d.jpg 
 
